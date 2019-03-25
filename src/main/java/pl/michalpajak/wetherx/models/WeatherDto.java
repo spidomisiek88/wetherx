7e081903b5398c2190eb687cd15e6f5c1209ff32
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-25 11:01:24 +0100

    weather dto

--- src/main/java/pl/michalpajak/wetherx/models/WeatherDto.java
+++ src/main/java/pl/michalpajak/wetherx/models/WeatherDto.java
@@ -1,12 +1,14 @@
 package pl.michalpajak.wetherx.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherDto {
 
-    public WeatherMain weatherMain;
+    @JsonProperty("main")
+    public WeatherMain mainData;
 
 }
--- src/main/java/pl/michalpajak/wetherx/models/services/WeatherService.java
+++ src/main/java/pl/michalpajak/wetherx/models/services/WeatherService.java
@@ -1,4 +1,31 @@
 package pl.michalpajak.wetherx.models.services;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pl.michalpajak.wetherx.models.WeatherDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class WeatherService {
+
+    public WeatherDto getWeather(String cityName) {
+        Map<String, String> urlVars = new HashMap<>();
+        urlVars.put("q", cityName);
+        urlVars.put("appid", "e9fcb4a9a22e01907b6742a720b67145");
+
+        WeatherDto weatherDto = getRestTemplate().getForObject("api.openweathermap.org/data/2.5/weather",
+                WeatherDto.class,
+                urlVars);
+
+        return weatherDto;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 }
